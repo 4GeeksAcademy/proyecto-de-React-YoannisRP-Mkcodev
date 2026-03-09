82efd64ae277651fc8fccfdfc854003555cdbc7e
Yoan <dafneangel3008@gmail.com>
2026-03-09 19:13:09 +0000

    Commit YoannisRP

---
+++ src/js/components/Hero.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className="p-5 mb-4 bg-light rounded-3">
+      <div className="container-fluid py-5">
+        <h1 className="display-5 fw-bold">A Warm Welcome!</h1>
+        <p className="col-md-8 fs-4">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </p>
+        <button className="btn btn-primary btn-lg">Call to Action!</button>
+      </div>
+    </div>
+
+  )
+}
+
+export default Hero
